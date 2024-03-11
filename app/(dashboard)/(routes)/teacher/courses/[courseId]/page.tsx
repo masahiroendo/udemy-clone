1813c027418e3296c47c5fd0cc9158f21190bcf7
matcha <massahiro.endo@gmail.com>
2024-03-11 10:24:00 +0100

    feat: create course page/routes title and description

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,65 @@
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+
+import { IconBadge } from "@/components/icon-badge";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="tex-sm text-muted-foreground">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} size="sm" />
+            <h2 className="text-xl">Cutstomize your course</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseIdPage;
