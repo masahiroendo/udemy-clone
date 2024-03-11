8f51ed7323dcd43e673c90083ef8fb273b942e20
matcha <massahiro.endo@gmail.com>
2024-03-11 10:24:43 +0100

    efat: uploadthing setup course image upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        // port: "",
+        // pathname: "/account123/**",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+        // port: "",
+        // pathname: "/account123/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
