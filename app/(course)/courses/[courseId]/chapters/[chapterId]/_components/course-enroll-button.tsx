d7371660a199066068dd5293137b3c2e56f7e518
matcha <massahiro.endo@gmail.com>
2024-03-19 13:05:15 +0100

    feat: stripe payment checkout route webhook route

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
+
+import { toast } from "sonner";
 import { formatPrice } from "@/lib/format";
 import { Button } from "@/components/ui/button";
 
@@ -12,8 +16,26 @@ export const CourseEnrollButton = ({
   courseId,
   price,
 }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <Button size="sm" className="w-full md:w-auto">
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size="sm"
+      className="w-full md:w-auto"
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
