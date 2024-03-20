e457591d079bb9a892cd0561e7b84bd1d18dc4ff
matcha <massahiro.endo@gmail.com>
2024-03-20 13:56:18 +0100

    fix: teacher mode 2

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,6 +1,6 @@
 export const isTeacher = (userId?: string | null) => {
   return (
-    userId === process.env.NEXT_PUBLIC_TEACHER_ID1 ||
-    process.env.NEXT_PUBLIC_TEACHER_ID2
+    userId ===
+    (process.env.NEXT_PUBLIC_TEACHER_ID1 || process.env.NEXT_PUBLIC_TEACHER_ID2)
   );
 };
